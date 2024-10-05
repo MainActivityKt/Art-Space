6504ad90f06f859dbe8f758fd951edf8fb83b99c
Safi <safiullahkakar2020@outlook.com>
2024-10-06 02:09:23 +0430

    Added vertical scroll support for large screens

--- app/src/main/java/com/practice/artspace/MainActivity.kt
+++ app/src/main/java/com/practice/artspace/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -61,7 +63,9 @@ fun ArtSpaceApp(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp)
+
     )
     {
         val listOfArtworks = arrayOf(
